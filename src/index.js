2290302a592324929789ffe8f64bce080e8714f2
SrKomodo <komodosr@gmail.com>
2018-04-07 17:39:11 -0300

    Use divs instead of canvas, makes everything smoother

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 const SIZE = 20;
 
-const bodyParts = new Array(10).fill(0);
-
-/**
- * Sets the appropiate size for the canvas
- */
-function setSize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
 
 // Setup canvas
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
-canvas.id = "flying-mouse-canvas";
-document.body.appendChild(canvas);
-setSize();
+const container = document.createElement("div");
+container.id = "flying-mouse-container";
+document.body.appendChild(container);
+
+// Setup bodyparts
+const angles = new Array(10).fill(0);
+for (let i = 0; i < 10; i++) {
+  const bodypart = document.createElement("div");
+  bodypart.style.width = bodypart.style.height = `${SIZE}px`;
+  container.appendChild(bodypart);
+}
 
 let dx = 0, dy = 0; // These variables will track the mouse movement
 let x = 0, y = 0; // And these ones will track the position
@@ -36,24 +33,24 @@ document.addEventListener("mousemove", (e) => {
  * @param {number} phi rotation in polar coordinates
  */
 function draw(r, phi) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   // This basically shifts all body parts back by 1 and adds the new one
-  bodyParts.pop();
-  bodyParts.unshift(phi);
+  angles.pop();
+  angles.unshift(phi);
 
   // With these variables we will accumulate the offset between body parts
   let offsetX = x, offsetY = y;
 
   // Now we just render each bodypart
-  for (const angle of bodyParts) {
-    ctx.save();
-    ctx.translate(offsetX, offsetY);
-    ctx.rotate(angle);
-    ctx.fillRect(-SIZE / 2, -SIZE / 2, SIZE, SIZE);
-    ctx.restore();
+  for (let i = 0; i < angles.length; i++) {
+    const angle = angles[i];
+    /** @type {HTMLDivElement} */
+    const element = container.children[i];
 
-    // And add to the offset
+    element.style.transform = `
+      translate(${offsetX - SIZE / 2}px, ${offsetY - SIZE / 2}px) 
+      rotate(${angle}rad)
+    `;
+    
     offsetX -= SIZE * Math.cos(angle);
     offsetY -= SIZE * Math.sin(angle);
   }
