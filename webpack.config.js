2746ceda3efa194343e7e42fced18ad6d0f10e4f
SrKomodo <komodosr@gmail.com>
2018-04-21 17:15:25 -0300

    fix sourcemaps breaking things on development

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     inject: "./src/inject.ts",
     popup: "./src/popup.ts"
   },
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
